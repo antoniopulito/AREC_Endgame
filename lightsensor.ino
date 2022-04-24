562a61a7ed09ab14e0e890ad88a5bc982a560018
Richard-Barakat <99231160+Richard-Barakat@users.noreply.github.com>
2022-04-23 17:13:25 -0700

    Add files via upload

---
+++ lightsensor.ino
@@ -0,0 +1,297 @@
+/* TSL2591 Digital Light Sensor */
+/* Dynamic Range: 600M:1 */
+/* Maximum Lux: 88K */
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+
+float full = 0;    // value of reading
+
+/* MQTT */
+char g_mqtt_message_buffer[150];    // General purpose buffer for MQTT messages
+char g_command_topic[50];           // MQTT topic for receiving commands
+
+#if REPORT_MQTT_SEPERATE
+char g_light1_mqtt_topic[50];        // MQTT topic for reporting temp1
+#endif
+#if REPORT_MQTT_JSON
+char g_mqtt_json_topic[50];         // MQTT topic for reporting all values using JSON
+#endif
+
+/****************************************************************************/
+// WIFI
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "AREC_WL";
+const char* password = "AREC_WL_PASSWD";
+const char* mqtt_server = "192.168.2.101";
+const char* mqtt_user = "eric";
+const char* mqtt_password = "eric";
+const char* mqtt_device_id = "Light_Sensor1";
+//const char* mqtt_lwt = "LAST_WILL_IF_DESIRED";
+const boolean mqtt_retain = 0; //set to 0 to resolve reconnect LOP issues
+const uint8_t mqtt_qos = 0; //can only be a value of (0, 1, 2)
+String rx;
+int rxLength = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+/****************************************************************************/
+
+// Example for demonstrating the TSL2591 library - public domain!
+
+// connect SCL to I2C Clock
+// connect SDA to I2C Data
+// connect Vin to 3.3-5V DC
+// connect GROUND to common ground
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+
+
+/**************************************************************************/
+/*
+    Configures the gain and integration time for the TSL2591
+*/
+/**************************************************************************/
+void configureSensor(void)
+{
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_LOW);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+
+
+  /* Display the gain and integration time for reference sake */
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Gain:         "));
+  tsl2591Gain_t gain = tsl.getGain();
+  switch (gain)
+  {
+    case TSL2591_GAIN_LOW:
+      Serial.println(F("1x (Low)"));
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println(F("25x (Medium)"));
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println(F("428x (High)"));
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println(F("9876x (Max)"));
+      break;
+  }
+  Serial.print  (F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC);
+  Serial.println(F(" ms"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+}
+
+
+/**************************************************************************/
+/*
+    Program entry point for the Arduino sketch
+*/
+/**************************************************************************/
+void setup(void)
+{
+  // Set up topic for publishing sensor readings
+  sprintf(g_light1_mqtt_topic, "tele/%x/LIGHT1",  ESP.getChipId());
+
+  Serial.begin(9600);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  Serial.println(F("Starting Adafruit TSL2591 Test!"));
+
+  if (tsl.begin())
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+  }
+  else
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1);
+  }
+
+  /* Configure the sensor */
+  configureSensor();
+
+  // Now we're ready to get readings ... move on to loop()!
+}
+
+// WIFI SETUP
+
+void setup_wifi() {
+  delay(10);  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  //WiFi.config(ip, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {\
+  
+  Serial.print("RX: ");
+  //Convert and clean up the MQTT payload messsage into a String
+  rx = String((char *)payload);                    //Payload is a Byte Array, convert to char to load it into the "String" object
+  rxLength = rx.length();                          //Figure out how long the resulting String object is
+  for (int i = length; i <= rxLength; i++)         //Loop through setting extra characters to null as garbage may be there
+  {
+    rx.setCharAt(i, ' ');
+  }
+  rx.trim();                                       //Use the Trim function to finish cleaning up the string
+  Serial.print(rx);                                //Print the recieved message to serial
+  Serial.println();
+}
+
+// RECONNECT TO MQTT IF YOU LOSE CONNECTION
+/*
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(g_light1_mqtt_topic, mqtt_user, mqtt_password)) {
+      //needs next line for proper connect to mqtt broker that doesn't allow anonymous
+      //if (client.connect(mqtt_device_id, mqtt_user, mqtt_password, mqtt_topic, mqtt_qos, mqtt_retain, mqtt_lwt)) {
+      Serial.println("connected");
+
+      // ... and resubscribe
+      client.subscribe(g_light1_mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+*/
+
+void reconnect() {
+  char mqtt_client_id[30];
+  sprintf(mqtt_client_id, "esp8266-%x", ESP.getChipId());
+
+  // Loop until we're reconnected
+  Serial.print("Connecting to MQTT Broker");
+  while (!client.connected()){
+    Serial.print(".");
+    if (client.connect(mqtt_client_id, mqtt_user, mqtt_password)){
+      Serial.println("Connected");
+      sprintf(g_mqtt_message_buffer, "Device %s starting up", mqtt_client_id);
+      client.publish(status_topic, g_mqtt_message_buffer);
+    } else{
+      delay(5000);
+    }
+  }
+}
+
+/**************************************************************************/
+/*
+    Shows how to perform a basic read on visible, full spectrum or
+    infrared light (returns raw 16-bit ADC values)
+*/
+/**************************************************************************/
+void simpleRead(void)
+{
+  // Simple data read example. Just read the infrared, fullspecrtrum diode
+  // or 'visible' (difference between the two) channels.
+  // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
+  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
+
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("Luminosity: "));
+  Serial.println(x, DEC);
+}
+
+/**************************************************************************/
+/*
+    Show how to read IR and Full Spectrum at once and convert to lux
+*/
+/**************************************************************************/
+void advancedRead(void)
+{
+  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  // That way you can do whatever math and comparisons you want!
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
+  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
+  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
+  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+}
+
+/**************************************************************************/
+/*
+    Performs a read using the Adafruit Unified Sensor API.
+*/
+/**************************************************************************/
+void unifiedSensorAPIRead(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  tsl.getEvent(&event);
+
+  /* Display the results (light is measured in lux) */
+  Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
+  if ((event.light == 0) |
+      (event.light > 4294966000.0) |
+      (event.light < -4294966000.0))
+  {
+    /* If event.light = 0 lux the sensor is probably saturated */
+    /* and no reliable data could be generated! */
+    /* if event.light is +/- 4294967040 there was a float over/underflow */
+    Serial.println(F("Invalid data (adjust gain or timing)"));
+  }
+  else
+  {
+    Serial.print(event.light); Serial.println(F(" lux"));
+  }
+}
+
+
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void)
+{
+  //simpleRead();
+  //unifiedSensorAPIRead();
+  // read temperature data
+  advancedRead();
+  if (!client.connected()) {
+    reconnect();
+  }
+  String message_string;
+  message_string = String(full);
+  message_string.toCharArray(g_mqtt_message_buffer, message_string.length()+1);
+  client.publish(g_light1_mqtt_topic, g_mqtt_message_buffer);
+  client.loop();
+  // sampling rate
+  delay(5000);
+}
